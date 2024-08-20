5fba9adaa5a74a94eae9b3b59b656c930d335f9b
nmatton <nicolas@locla.be>
2024-08-20 11:12:40 +0200

    adding loadBugReportSQL

--- modules/php/DebugTrait.php
+++ modules/php/DebugTrait.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace FOO;
+
 use FOO\Core\Globals;
 use FOO\Core\Notifications;
 use FOO\Core\Preferences;
@@ -9,7 +11,37 @@ use FOO\Managers\Players;
 
 trait DebugTrait
 {
-  function test()
+  function test() {}
+
+  public function loadBugReportSQL(int $reportId, array $studioPlayers): void
   {
+    $prodPlayers = $this->getObjectListFromDb("SELECT `player_id` FROM `player`", true);
+    $prodCount = count($prodPlayers);
+    $studioCount = count($studioPlayers);
+    if ($prodCount != $studioCount) {
+      throw new BgaVisibleSystemException("Incorrect player count (bug report has $prodCount players, studio table has $studioCount players)");
+    }
+
+    // SQL specific to your game
+    // For example, reset the current state if it's already game over
+    $sql = [
+      "UPDATE `global` SET `global_value` = 10 WHERE `global_id` = 1 AND `global_value` = 99"
+    ];
+    foreach ($prodPlayers as $index => $prodId) {
+      $studioId = $studioPlayers[$index];
+      // SQL common to all games
+      $sql[] = "UPDATE `player` SET `player_id` = $studioId WHERE `player_id` = $prodId";
+      $sql[] = "UPDATE `global` SET `global_value` = $studioId WHERE `global_value` = $prodId";
+      $sql[] = "UPDATE `stats` SET `stats_player_id` = $studioId WHERE `stats_player_id` = $prodId";
+
+      // SQL specific to your game
+      // TODO: update the sql below according to changes specific to your game
+      $sql[] = "UPDATE cards SET card_location='hand_" . $studioId . "' WHERE card_location='hand_" . $prodId . "'";
+      $sql[] = "UPDATE user_preferences SET player_id=$studioId WHERE player_id=$prodId";
+    }
+    foreach ($sql as $q) {
+      $this->DbQuery($q);
+    }
+    $this->reloadPlayersBasicInfos();
   }
 }
